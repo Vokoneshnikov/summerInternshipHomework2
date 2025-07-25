776d8ccbb8396021e85480c473f0a946e2838bd7
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-07-25 15:45:25 +0300

    final commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+﻿import "./styles.css";
+
+import { useState } from "react";
+
+export default function App() {
+    const [name, setName] = useState("");
+
+    const handleInputChange = (nm) => {
+        setName(nm.target.value);
+        console.log(`Имя изменено на ${nm.target.value}`);
+    };
+    const handleReset = () => {
+        setName("");
+        console.log("имя удалено");
+    };
+    return (
+        <div className="App">
+            <h1>Ваше имя: {name}</h1>
+            <input
+                type="text"
+                placeholder="Введите ваше имя"
+                onChange={handleInputChange}
+                value={name}
+            />
+            <button onClick={handleReset}>Сбросить</button>
+        </div>
+    );
+}
